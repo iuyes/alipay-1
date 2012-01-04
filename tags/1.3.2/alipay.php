16708e93bb657e90af44ba7726cff407db53f475
waisir <waisir@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-01-04 18:05:07 +0000

    git-svn-id: https://plugins.svn.wordpress.org/alipay@484615 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ tags/1.3.2/alipay.php
@@ -0,0 +1,31 @@
+<?php 
+/*
+	Plugin Name:集成支付宝
+	Plugin URI:http://www.waisir.com/wp-alipay
+	Description:<strong>集成支付宝</strong>,正在开发中...插件开发基本完成,但由于插件的特殊性,安全漏洞尚未修复,由于中途接到一个比较急的项目,被迫延期发布.
+	Version:1.0.1
+	Author:歪SIR
+	Author URI:http://www.waisir.com
+	License: GPLv2 or later
+*/
+ 
+/* 
+	Copyright (c) 2011-2012  waisir (Email : waisir@qq.com)
+	
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2 of the License, or
+	(at your option) any later version.
+	
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+	License: http://www.gnu.org/licenses/gpl.txt
+	
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+ 
+?>
