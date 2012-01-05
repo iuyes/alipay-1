7b4dc87d9dd207a37f695694fd12f0bae7208590
waisir <waisir@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-01-05 17:04:18 +0000

    git-svn-id: https://plugins.svn.wordpress.org/alipay@485172 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ tags/1.3.4/alipay.php
@@ -0,0 +1,40 @@
+<?php 
+/*
+Plugin Name:集成支付宝
+Plugin URI:http://www.waisir.com/wp-alipay
+Description:<strong>集成支付宝</strong>，可以在后台添加商品信息，并在相应页面输入短代码直接调用商品
+Version:1.3.4
+Author:歪SIR
+Author URI:http://www.waisir.com
+License: GPLv2 or later
+*/
+ 
+/* 
+Copyright (c) 2011-2012  waisir (Email : waisir@qq.com)
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+License: http://www.gnu.org/licenses/gpl.txt
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+ 
+
+require_once('includes/cfg.config.php');
+require_once('includes/fnc.core.php');
+require_once('includes/inc.actions.php');
+require_once('includes/cls.widgets.php');
+
+
+
+
+?>
